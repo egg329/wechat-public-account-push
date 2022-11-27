3eabd16120897f9beecd6196e5c6e47fb329b1d1
egg329 <119155917+egg329@users.noreply.github.com>
2022-11-27 18:02:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1998', date: '09-31',
+          type: '*生日', name: '岑老师', year: '1999', date: '03-19',
         },
         {
           type: '节日', name: '恋爱纪念日', year: '2022', date: '10-04',
